080741f3767a6c8e556d15a64d2f8152b3427d21
denis.saraev <rads3@ya.ru>
2023-03-19 19:18:50 +0400

    refactor

---
+++ lesson3/lesson3_func3.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	dollar := 30
+
+	getDollarValue := func() int {
+		return dollar
+	}
+
+	fmt.Println(getDollarValue())
+	dollar = 70
+
+	fmt.Println(getDollarValue())
+}
